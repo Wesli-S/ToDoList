225f6db3206d257c8144156bc24730dfe1cc10e3
Yumi Smart <wesli.smart1017@gmail.com>
2023-11-10 11:31:02 -0800

    Added main functionality code

---
+++ js/main.js
@@ -0,0 +1,21 @@
+class List {
+}
+window.onload = function () {
+    let addBtn = document.querySelector("#add-button");
+    addBtn.onclick = addToList;
+};
+function addToList() {
+    let item = document.querySelector("#item").value;
+    let newListItem = new List();
+    newListItem.item = item;
+    let itemDiv = document.createElement("div");
+    let listItem = document.createElement("p");
+    itemDiv.appendChild(listItem);
+    let divDisplay = document.querySelector("#display");
+    divDisplay.appendChild(itemDiv);
+    itemDiv.onclick = itemDivClicked;
+}
+function itemDivClicked() {
+    let currItemDiv = this;
+    currItemDiv.classList.toggle("finished");
+}
---
+++ ts/main.ts
@@ -0,0 +1,44 @@
+class List {
+    item:string;
+}
+
+window.onload = function(){
+    let addBtn = document.querySelector("#add-button") as HTMLButtonElement;
+    addBtn.onclick = addToList;
+}
+
+function addToList(){
+    //Get data from form
+    let item = (document.querySelector("#item") as HTMLInputElement).value;
+
+    //Populate the object
+    let newListItem = new List();
+    newListItem.item = item;
+    //Display new list item w. interaction
+    /*
+        TO DO
+        - List Item
+        - List Item
+        - List Item
+    */
+
+    /*Formatting how the list pops up */
+    //create elements/add text
+    let itemDiv = document.createElement("div");
+    let listItem = document.createElement("p");
+    //add elements to div
+    itemDiv.appendChild(listItem);
+
+    //Add item to page
+    let divDisplay = document.querySelector("#display") as HTMLDivElement;
+    divDisplay.appendChild(itemDiv);
+    //make items in the list interactive (clicking on a list item should make it turn green)
+    itemDiv.onclick = itemDivClicked;
+}
+
+function itemDivClicked(){
+    // The "this" keyword will hold the <div> element
+    // that triggered this function call
+    let currItemDiv = this as HTMLDivElement;
+    currItemDiv.classList.toggle("finished");
+}
