d11c5097e5772ba8046c2ee80f2e6b6d6d36a196
Yumi Smart <wesli.smart1017@gmail.com>
2023-11-10 14:12:14 -0800

    Added web storage, saving all input data to the page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-class List {
+class ToDo {
 }
 window.onload = function () {
     let addBtn = document.querySelector("#add-button");
@@ -6,17 +6,29 @@ window.onload = function () {
 };
 function addToList() {
     let item = document.querySelector("#item").value;
-    let newListItem = new List();
+    let newListItem = new ToDo();
     newListItem.item = item;
     let itemDiv = document.createElement("div");
-    let listItem = document.createElement("li");
+    let listItem = document.createElement("p");
     listItem.textContent = newListItem.item;
     itemDiv.appendChild(listItem);
     let divDisplay = document.querySelector("#display");
     divDisplay.appendChild(itemDiv);
     itemDiv.onclick = itemDivClicked;
+    addItemToStorage();
 }
 function itemDivClicked() {
     let currItemDiv = this;
     currItemDiv.classList.toggle("finished");
+}
+function addItemToStorage() {
+    const ListStorageKey = "List";
+    let toDoItems = [];
+    const itemData = localStorage.getItem(ListStorageKey);
+    if (itemData !== null) {
+        toDoItems = JSON.parse(itemData);
+    }
+    let item = document.querySelector("#item").value;
+    toDoItems.push(item);
+    localStorage.setItem(ListStorageKey, JSON.stringify(toDoItems));
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,4 +1,4 @@
-class List {
+class ToDo {
     item:string;
 }
 
@@ -7,12 +7,15 @@ window.onload = function(){
     addBtn.onclick = addToList;
 }
 
+/**
+ * Adds user input to a bulleted list that appears under the "To Do" header
+ */
 function addToList(){
     //Get data from form
     let item = (document.querySelector("#item") as HTMLInputElement).value;
 
     //Populate the object
-    let newListItem = new List();
+    let newListItem = new ToDo();
     newListItem.item = item;
     //Display new list item w. interaction
     /*
@@ -27,7 +30,7 @@ function addToList(){
     let itemDiv = document.createElement("div");
     let listItem = document.createElement("p");
     listItem.textContent = newListItem.item; //makes the item visible
-    
+
     //add elements to div
     itemDiv.appendChild(listItem);
 
@@ -36,11 +39,38 @@ function addToList(){
     divDisplay.appendChild(itemDiv);
     //make items in the list interactive (clicking on a list item should make it turn green)
     itemDiv.onclick = itemDivClicked;
+    addItemToStorage();
 }
 
+/**
+ * Activates the added div, and adds the user input to the list
+ */
 function itemDivClicked(){
     // The "this" keyword will hold the <div> element
     // that triggered this function call
     let currItemDiv = this as HTMLDivElement;
     currItemDiv.classList.toggle("finished");
+}
+
+/*WEB STORAGE TIIIIIIIIIIIIIME*/
+
+function addItemToStorage() {
+
+    const ListStorageKey = "List";
+    let toDoItems: string[] = [];
+
+    //read existing list items from storage
+    const itemData = localStorage.getItem(ListStorageKey);
+
+    if(itemData !== null) {
+        toDoItems = JSON.parse(itemData);
+    }
+
+    //add current item to the list
+    let item = (<HTMLInputElement>document.querySelector("#item")).value;
+    toDoItems.push(item);
+
+    //Store the modified list back into storage
+    localStorage.setItem(ListStorageKey, JSON.stringify(toDoItems));
+
 }
